22b7eb120dbd741d76c74e95577fb861bc39ce7d
Illarion Panchenko <32985776+illarionPanchenko@users.noreply.github.com>
2019-04-24 11:41:19 +0300

    icons

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ export default class App extends Component{
     city: undefined,
     country: undefined,
     humidity: undefined,
-    description: undefined,
+    description: "",
     error: undefined
 };
 
--- src/weather.js
+++ src/weather.js
@@ -2,6 +2,10 @@ import React from 'react';
 import './App.css'
 
 const Weather = (props) => {
+
+    let condition = "";
+
+  if(props.description) {condition = props.description.replace(/\s+/g, '');}else{condition = ""}
     return(
         <div className="weather">
             {props.city&&props.country && <p>  Location: {props.city}, {props.country}</p>}
@@ -9,6 +13,7 @@ const Weather = (props) => {
             {props.humidity && <p> Humidity: {props.humidity}%</p>}
             {props.description && <p>Condition: {props.description}</p>}
             {props.error && <h2>{props.error}</h2>}
+            <div className={condition}> </div>
         </div>
     )
 } ;
