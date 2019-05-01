d9b0b06f0c16e8c1d040502ccd0935cce444079e
Illarion Panchenko <32985776+illarionPanchenko@users.noreply.github.com>
2019-05-01 15:15:00 +0300

    +google map api

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import Weather from './weather';
 import "./App.css"
 
 const API_KEY = 'e3e936e33460527ab6c431786272fe98';
+//const API_FORECAST = 'e99130a37c62880a1bc1159777950b81';
+
 
 export default class App extends Component{
 
@@ -14,7 +16,10 @@ export default class App extends Component{
     country: '',
     humidity: undefined,
     description: "",
-    error: undefined
+    error: undefined,
+      lat: 50,
+      lng: 30,
+      zoom: 2
 };
 
   getWeather = async (e) => {
@@ -25,6 +30,14 @@ export default class App extends Component{
     const api_call = await
         fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}${country}&appid=${API_KEY}`);
     const data = await api_call.json();
+
+   /*  const api_call_forecast = await
+          fetch(`api.openweathermap.org/data/2.5/forecast?q=${city}${country}&appid=${API_FORECAST}`);
+      const dataForecast = await api_call_forecast.list[0].json();
+      console.log(dataForecast);*/
+
+   // console.log(data);////////////////////////////////////////////////////////////////////////////////////////////
+
     if (city) {
     this.setState({
       temperature: Math.round(data.main.temp - 273),
@@ -32,8 +45,13 @@ export default class App extends Component{
       country: data.sys.country,
       humidity: data.main.humidity,
       description: data.weather[0].description,
-      error: undefined
-    })} else {
+      error: undefined,
+        lat: data.coord.lat,
+        lng: data.coord.lon,
+        zoom: 10
+    });
+        window.initMap()
+    } else {
       this.setState({
         temperature:undefined,
         city: undefined,
@@ -46,12 +64,18 @@ export default class App extends Component{
   };
 
 
+
   render() {
+
     return(
         <div className='App'>
           <div className='block'>
               <div className="title">
-          <Title />
+          <Title
+              lat = {this.state.lat}
+              lng = {this.state.lng}
+              zoom={this.state.zoom}
+          />
               </div>
           <div className="form1">
           <Form getWeather={this.getWeather}/>
--- src/title.js
+++ src/title.js
@@ -1,12 +1,42 @@
 import React from 'react';
+import './App.css';
 
 
-const Title = () => {
-    return(
-        <div>
-            <h1>Weather finder</h1>
-            <p>Find out temperatures, conditions and more...</p>
-        </div>
-    )
+export default class Title extends React.Component {
+
+    componentDidMount() {
+        this.renderMap();
+    }
+
+    renderMap =()=>{
+      loadScript("https://maps.googleapis.com/maps/api/js?key=AIzaSyCfFsG0Hk-GrF4xv9K5qOgJO-Qq0-bBtU4&callback=initMap"
+      );
+        window.initMap = this.initMap
+    };
+
+    initMap = () => {
+        const map = new window.google.maps.Map(document.getElementById('map'), {
+            center: {lat: this.props.lat, lng: this.props.lng},
+            zoom: this.props.zoom
+        });
+
+    };
+
+    render() {
+
+        return (
+            <main>
+                <div id="map"></div>
+            </main>
+        )
+    }
 }
-export default Title;
+
+    function loadScript(url) {
+        var index = window.document.getElementsByTagName("script")[0];
+        var script = window.document.createElement('script');
+        script.src = url;
+        script.async = true;
+        script.defer = true;
+        index.parentNode.insertBefore(script,index)
+    }
